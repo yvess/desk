8f77425da9867c29ee0f034b70283e9cda0a777d
Yves Serrano <y@yas.ch>
2012-08-06 21:21:59 +0200

    added custom couchdb upload for json files with new lines

---
+++ desk/utils/__init__.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+from __future__ import division
+#from __future__ import unicode_literals
+from __future__ import print_function
+
+import sys
+sys.path.append("../")
+
+import requests
+
+
+class CouchdbUploader(object):
+    def __init__(self, couchdb_uri=None, couchdb_db=None, path=None):
+        self.uri = couchdb_uri
+        self.db = couchdb_db
+        self.path = path
+
+    def put(self, data, doc_id):
+        if data[0] == "@":
+            filename = "{}/{}".format(self.path, data[1:])
+            with open(filename, "r") as f:
+                data = "".join([l.strip() for l in f.readlines()])
+        r = requests.put("{}/{}/{}".format(self.uri, self.db, doc_id.format(couchdb_db=self.db)), data=data,
+            headers={'Content-type': 'application/json', 'Accept': 'text/plain'}
+        )
+        return r.status_code
