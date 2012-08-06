ebda537ca8f9c4a2c4e712fab1f3812e66ac2fa8
Yves Serrano <y@yas.ch>
2012-08-06 21:20:39 +0200

    added config parser and ini file support for settings

--- desk/pluginbase/dns.py
+++ desk/pluginbase/dns.py
@@ -1,3 +1,9 @@
+# coding: utf-8
+from __future__ import division
+from __future__ import unicode_literals
+from __future__ import print_function
+
+
 import abc
 
 
--- desk/worker.py
+++ desk/worker.py
@@ -1,50 +1,62 @@
-#from couchdbkit import Server, Consumer
-#s = Server()
-#db = s['desk_drawer']
-#c = Consumer(db)
-#def print_line(line):
-#    print "got %s" % line
-#c.wait(print_line,since=0) # Go into receive loop
-
+#!/usr/bin/env python
+# coding: utf-8
+from __future__ import division
+from __future__ import unicode_literals
+from __future__ import print_function
 
 import sys
 import os
-from couchdbkit import Server
-from couchdbkit.changes import ChangesStream
-#import desk.plugin
-sys.path.append("/home/yserrano/code/cappuccino/desk")
-from desk.plugin import dns
+from ConfigParser import SafeConfigParser
+from ConfigParser import ParsingError
+import argparse
+sys.path.append("../")
+from desk import Worker
+from deks.utls import put_json
 
-s = Server(uri="http://dev1.taywa.net:5984")
-desk_drawer = s['desk_drawer']
 
-#settings = desk_drawer.get("worker-settings")
+def setup_parser():
+    """create the command line parser / config file reader """
 
-hostname = os.uname()[1]
-provides = {}
+    defaults = {
+        "couchdb_uri": "http://localhost:5984",
+        "couchdb_db": "desk_drawer",
+    }
+    # first only parse the config file argument
+    conf_parser = argparse.ArgumentParser(add_help=False)
+    conf_parser.add_argument("-c", "--config", dest="config",
+        default="/etc/desk/worker.conf",
+        help="path to the config file, default: /etc/desk/worker.conf",
+        metavar="FILE"
+    )
+    args, remaining_args = conf_parser.parse_known_args()
+    # load config files with settings, puts them into a dict format "section_option"
+    if args.config:
+        config = SafeConfigParser()
+        if not config.read([args.config]):
+            print("Can't open file '{}'".format(args.config))
+            sys.exit(0)
+        else:
+            for section in ['couchdb']:  # put in here all your sections
+                defaults.update(
+                    {'{}_{}'.format(section, k):v for k, v in config.viewitems(section)}
+                )
+    # parse all other arguments
+    parser = argparse.ArgumentParser(
+        parents=[conf_parser],
+        description="""Worker is the agent for the desk plattform.
+                       Command line switches overwrite config file settings""",
+    )
+    parser.set_defaults(**defaults)
+    parser.add_argument("-u", "--couchdb_uri", dest="couchdb_uri",
+        metavar="URI", help="connection url of the server")
+    parser.add_argument("-d", "--couchdb_db", dest="couchdb_db",
+        metavar="NAME", help="database of the server")
 
-# setup worker
-for worker in desk_drawer.list("desk_drawer/list_docs", "worker", include_docs=True, startkey=hostname, endkey=hostname):
-    provides = worker['provides']
-print provides
+    args = parser.parse_args(remaining_args)
 
-with ChangesStream(desk_drawer, feed="continuous", heartbeat=True, filter="desk_drawer/queue") as queue:
-    for job in queue:
-        print job
-        for item in desk_drawer.view('desk_drawer/todo'):
-            doc = item['value']
-            if doc['type'] in provides:
-                for service_settings in provides[doc['type']]:
-                    if 'server_type' in service_settings and 'master' in service_settings['server_type'] or not 'server_type' in service_settings:
-                        print doc['type'], service_settings['backend']
-                        ServiceClass = None
-                        doc_type = doc['type']
-                        backend = service_settings['backend']
-                        try:
-                            ServiceClass = getattr(getattr(globals()[doc_type], backend), backend.title())
-                        except AttributeError:
-                            print "not found"
-                        if ServiceClass:
-                            print type(ServiceClass), ServiceClass
-                            service = ServiceClass(doc)
-                            service.update()
+    return args
+
+if __name__ == "__main__":
+    settings = setup_parser()
+    worker = Worker(settings)
+    worker.run()
