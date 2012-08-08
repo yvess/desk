21a9c0cf391e13377bafaa7320b6de34eee4ec86
Yves Serrano <y@yas.ch>
2012-08-08 02:11:57 +0200

    added ObjectDict class, fixed json multine docs

--- desk/utils/__init__.py
+++ desk/utils/__init__.py
@@ -9,6 +9,11 @@ sys.path.append("../")
 import requests
 
 
+class ObjectDict(object):
+    def __init__(self, **entries):
+        self.__dict__.update(entries)
+
+
 class CouchdbUploader(object):
     def __init__(self, couchdb_uri=None, couchdb_db=None, path=None):
         self.uri = couchdb_uri
@@ -19,7 +24,7 @@ class CouchdbUploader(object):
         if data[0] == "@":
             filename = "{}/{}".format(self.path, data[1:])
             with open(filename, "r") as f:
-                data = "".join([l.strip() for l in f.readlines()])
+                data = "".join([l.strip() for l in f.readlines() if not l.strip().find("//") == 0])  # remove comments in json
         r = requests.put("{}/{}/{}".format(self.uri, self.db, doc_id.format(couchdb_db=self.db)), data=data,
             headers={'Content-type': 'application/json', 'Accept': 'text/plain'}
         )
