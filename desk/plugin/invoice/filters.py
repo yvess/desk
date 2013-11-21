6eddbf5b56e04ff70dd186822d4814e111b96c97
Yves Serrano <y@yas.ch>
2013-11-21 18:10:24 +0100

    better nl2br filter, only br no p

--- desk/plugin/invoice/filters.py
+++ desk/plugin/invoice/filters.py
@@ -10,8 +10,8 @@ _paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
 
 @evalcontextfilter
 def nl2br(eval_ctx, value):
-    result = u'\n\n'.join(u'<p>%s</p>' % p.replace('\n', '<br>\n')
-                          for p in _paragraph_re.split(escape(value)))
+    _paragraph_re = re.compile(r'(?:\r\n|\r(?!\n)|\n){2,}')
+    result = u'\n\n'.join(u'%s   ' % p.replace(u'\r\n', u'<br/>') for p in _paragraph_re.split(value))
     if eval_ctx.autoescape:
         result = Markup(result)
     return result
