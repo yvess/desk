72af6d38f6d86d61f95d65c0eca5559aef531446
Yves Serrano <y@yas.ch>
2015-01-09 23:17:45 +0100

    register worker

--- desk/docker/worker/docker-entrypoint.sh
+++ desk/docker/worker/docker-entrypoint.sh
@@ -3,18 +3,25 @@
 
 if [ "$1" = 'worker' ]; then
   if [ ! -f "/var/py27/lib/python2.7/site-packages/desk.egg-link" ]; then
-    echo "adding desk to python packages"
+    echo "* adding desk to python packages"
     cd /projects/desk && /var/py27/bin/python setup.py develop
   fi
   WORKER_TYPE=${WORKER_TYPE:-worker} # set to worker as default
   if [ $WORKER_TYPE = "foreman" ]; then
-    echo "setup foreman"
+    echo "* setup foreman"
     wget -q --retry-connrefused -t 5 http://cdb:5984/ # wait for couchdb
-    curl --head --silent -u admin:admin http://cdb:5984/desk_drawer | egrep "200 OK|401"
+    # creating desk_drawer database
+    curl -Is -u admin:admin http://cdb:5984/desk_drawer | egrep -q "HTTP.*200|HTTP.*401"
     if [ $? -ne 0 ]; then # db desk_drawer does not exist
-        echo "creating desk_drawer database"
         cd /projects/desk/desk && /var/py27/bin/python ./dworker install-db
+        echo "* created desk_drawer database"
     fi
+  fi
+  # register worker
+  curl -Is -u admin:admin http://cdb:5984/desk_drawer/worker-`hostname` | egrep -q "HTTP.*404"
+  if [ $? -eq 0 ]; then # worker doesn't exist
+    cd /projects/desk/desk && /var/py27/bin/python ./dworker install-worker
+    echo "* registred worker"
   fi
   exec /usr/sbin/runsvdir-start
 fi
