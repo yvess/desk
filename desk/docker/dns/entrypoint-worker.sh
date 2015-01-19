b5b8f2c0f439db1898e23b48472023bfbe0db7aa
Yves Serrano <y@yas.ch>
2015-01-19 23:35:29 +0100

    don't hardcode /var/services

--- desk/docker/dns/entrypoint-worker.sh
+++ desk/docker/dns/entrypoint-worker.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
 if [ -d "/root/build" ]; then
-  # SETUP WORKER SCRIPT
+  # PREPARE STUFF
   echo "* setup dns"
-  IP=`hostname -i`
+  IP=$(hostname -i)
+  PDNS_DATA=${PDNS_DATA:-/var/services/data/powerdns}
+  rm /etc/powerdns/pdns.d/pdns.local.conf
 
   # CONFIGURE PDNS
   if [ ! -f "/etc/powerdns/pdns.d/pdns.local.conf" ]; then
     mv /root/build/pdns.local.conf /root/build/pdns.local.gsqlite3.conf /etc/powerdns/pdns.d/
+    sed -i -e "s#-PDNS_DATA-#${PDNS_DATA}#" \
+      /etc/powerdns/pdns.d/pdns.local.gsqlite3.conf
     sed -i -e "s/-IP-/${IP}/" -e "s/-HOSTNAME-/${HOSTNAME}/" \
       /etc/powerdns/pdns.d/*
     rm /etc/powerdns/bindbackend.conf /etc/powerdns/pdns.d/pdns.simplebind.conf
   fi
 
   # SETUP PDNS DATABASE
-  if [ -f "/var/services/data/powerdns/pdns_`hostname`.sqlite3" ]; then
-    sqlite3 /var/services/data/powerdns/pdns_`hostname`.sqlite3 < /root/build/powerdns-setup.sql
+  if [ -f "$PDNS_DATA/pdns_$(hostname).sqlite3" ]; then
+    sqlite3 $PDNS_DATA/pdns_$(hostname).sqlite3 < /root/build/powerdns-setup.sql
   fi
 fi
