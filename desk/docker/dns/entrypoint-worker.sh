5de7231c4fb5650ced8df36edab3bcdb869c9b99
Yves Serrano <y@yas.ch>
2015-01-19 22:57:59 +0100

    refactored entrypoint files

--- desk/docker/dns/entrypoint-worker.sh
+++ desk/docker/dns/entrypoint-worker.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 if [ -d "/root/build" ]; then
+  # SETUP WORKER SCRIPT
   echo "* setup dns"
   IP=`hostname -i`
-  # move & delete some files
-  mv /root/build/service/pdns /etc/service/pdns
-  mv /root/build/pdns.local.conf /root/build/pdns.local.gsqlite3.conf /etc/powerdns/pdns.d/
-  sed -i -e "s/-IP-/${IP}/" -e "s/-HOSTNAME-/${HOSTNAME}/" \
-    /etc/powerdns/pdns.d/*
-  rm /etc/powerdns/bindbackend.conf /etc/powerdns/pdns.d/pdns.simplebind.conf
-  # setup sqlitedatabase
+
+  # CONFIGURE PDNS
+  if [ ! -f "/etc/powerdns/pdns.d/pdns.local.conf" ]; then
+    mv /root/build/pdns.local.conf /root/build/pdns.local.gsqlite3.conf /etc/powerdns/pdns.d/
+    sed -i -e "s/-IP-/${IP}/" -e "s/-HOSTNAME-/${HOSTNAME}/" \
+      /etc/powerdns/pdns.d/*
+    rm /etc/powerdns/bindbackend.conf /etc/powerdns/pdns.d/pdns.simplebind.conf
+  fi
+
+  # SETUP PDNS DATABASE
   if [ -f "/var/services/data/powerdns/pdns_`hostname`.sqlite3" ]; then
-    sqlite3 /var/services/data/powerdns//pdns_`hostname`.sqlite3 < /root/build/powerdns-setup.sql
+    sqlite3 /var/services/data/powerdns/pdns_`hostname`.sqlite3 < /root/build/powerdns-setup.sql
   fi
 fi
--- desk/docker/worker/docker-entrypoint.sh
+++ desk/docker/worker/docker-entrypoint.sh
@@ -2,14 +2,17 @@
 #set -e
 
 if [ "$1" = 'worker' ]; then
+  # ADDING DESK TO PYTHON PATH
   if [ ! -f "/var/py27/lib/python2.7/site-packages/desk.egg-link" ]; then
     echo "* adding desk to python packages"
-    cd /opt/app && /var/py27/bin/python setup.py develop
+    cd /opt/app && /var/py27/bin/python setup.py develop > /dev/null
   fi
   WORKER_TYPE=${WORKER_TYPE:-worker} # set to worker as default
+
+  # RUN STUFF FOR FOREMAN, CREATE DATABASE
   if [ $WORKER_TYPE = "foreman" ]; then
     echo "* setup foreman"
-    wget -q --retry-connrefused -t 5 http://cdb:5984/ # wait for couchdb
+    wget -q --retry-connrefused -t 10 http://cdb:5984/ # wait for couchdb to get up
     # creating desk_drawer database
     curl -Is -u admin:admin http://cdb:5984/desk_drawer | egrep -q "HTTP.*200|HTTP.*401"
     if [ $? -ne 0 ]; then # db desk_drawer does not exist
@@ -17,16 +20,34 @@ if [ "$1" = 'worker' ]; then
         echo "* created desk_drawer database"
     fi
   fi
-  # setup worker
+
+  # RUN SETUP WORKER SCRIPT
   if [ -f "/entrypoint-worker.sh" ]; then
     /entrypoint-worker.sh
   fi
-  # register worker
+
+  # EDIT WORKER.CONF
+  if grep -qv -e "-HOSTNAME-" /etc/desk/worker.conf; then
+    echo "* update worker.conf"
+    sed -i -e "s/-HOSTNAME-/`hostname`/" /etc/desk/worker.conf
+  fi
+
+  # REGISTER WORKER
+  # wait for db created
+  until $(curl -Is -u admin:admin http://cdb:5984/desk_drawer|egrep -q "HTTP.*200"); do
+    sleep 1
+    echo "* wait desk_drawer db"
+  done
   curl -Is -u admin:admin http://cdb:5984/desk_drawer/worker-`hostname` | egrep -q "HTTP.*404"
   if [ $? -eq 0 ]; then # worker doesn't exist
     cd /opt/app/desk && /var/py27/bin/python ./dworker install-worker
     echo "* registred worker"
   fi
+
+  # ACTIVATE RUNIT SERVICES
+  [ ! -d "/etc/service/worker" ] && mv /root/build/service/* /etc/service/
+  # cleanup
+  rm -Rf /root/build
   exec /usr/sbin/runsvdir-start
 fi
 
