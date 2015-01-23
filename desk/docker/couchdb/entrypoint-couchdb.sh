688987137590d1a6256a18b0d94e215545f55a96
Yves Serrano <y@yas.ch>
2015-01-23 14:15:02 +0100

    fixed wrong variable name

--- desk/docker/couchdb/entrypoint-couchdb.sh
+++ desk/docker/couchdb/entrypoint-couchdb.sh
@@ -2,7 +2,7 @@
 
 if [ -d "/root/build" ]; then
   # SET DEFAULT VALUES IF NOT SET
-  VHOST_COUCHDB=${COUCHDB_DATA:-"desk.docker:5984"}
+  VHOST_COUCHDB=${VHOST_COUCHDB:-"desk.docker:5984"}
 
   # CONFIGURE PDNS
   if [ ! -f "/etc/couchdb/local.d/desk.ini" ]; then
