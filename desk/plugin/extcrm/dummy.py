c846dcedb0ecc0c9e8352b5b9f52a4d50e4d2624
Yves Serrano <y@yas.ch>
2015-10-16 14:01:44 +0200

    needing dict for dummy address

--- desk/plugin/extcrm/dummy.py
+++ desk/plugin/extcrm/dummy.py
@@ -5,4 +5,13 @@ from desk.plugin.extcrm.extcrmbase import ExtCrmBase
 
 class Dummy(ExtCrmBase):
     def get_address(self, pk=None):
-        return "dummy address"
+        address = {
+            'company_title': "Muster AG",
+            'firstname': "Hans",
+            'lastname': "Müller",
+            'address_street': "Meine Strasse 5",
+            'country_iso_alpha2': "CH",
+            'address_zip': "8000",
+            'address_city': "Zürich"
+        }
+        return address
