6f01725549344806b0349f70ba7f5e41b1243b11
Yves Serrano <y@yas.ch>
2016-04-25 15:20:49 +0200

    refactored to s6 moved files into etc folder

--- desk/docker/dns/entrypoint-worker.sh
+++ desk/docker/dns/entrypoint-worker.sh
@@ -2,46 +2,32 @@
 
 if [ -d "/root/build" ]; then
   # PREPARE STUFF
-  echo "* setup dns"
-  IP=$(hostname -i)
+  echo "* setup pdns"
+  HOST_IP=$(hostname -i)
   PDNS_DATA=${PDNS_DATA:-/var/services/data/powerdns}
   PDNS_LOG=${PDNS_LOG:-/var/services/log/powerdns}
   DNS_PRIMARY=${DNS_PRIMARY:-$HOSTNAME}
   mkdir -p "$PDNS_DATA" "$PDNS_LOG"
 
   # CONFIGURE WORKER
-  if [ ! -f "/etc/desk/worker.conf" ]; then
+  if [ grep "PDNS_DATA" "/etc/desk/worker.conf" ]; then
     echo "* configure worker.conf"
-    cp /root/build/etc/worker.conf /etc/desk/worker.conf
     sed -i -e "s#-HOSTNAME-#${HOSTNAME}#" -e "s#-DNS_PRIMARY-#${DNS_PRIMARY}#" \
         -e "s#-PDNS_DATA-#${PDNS_DATA}#" \
-      /etc/desk/worker.conf
+        /etc/desk/worker.conf
   fi
 
   # CONFIGURE PDNS
-  if [ ! -f "/etc/pdns.d/pdns.local.conf" ]; then
+  if [ grep "/etc/pdns.d/pdns.local.conf" "HOST_IP" ]; then
     echo "* configure pdns"
-    cp /root/build/pdns.conf /etc/pdns.conf
-    mkdir -p /etc/pdns.d/
-    cp /root/build/pdns.local.* /etc/pdns.d/
     sed -i -e "s#-PDNS_DATA-#${PDNS_DATA}#" \
-      /etc/pdns.d/pdns.local.gsqlite3.conf
-    sed -i -e "s/-IP-/${IP}/" -e "s/-HOSTNAME-/${HOSTNAME}/" \
-      /etc/pdns.d/*
+        /etc/pdns.d/pdns.local.gsqlite3.conf
+    sed -i -e "s/-HOST_IP-/${HOST_IP}/" -e "s/-HOSTNAME-/${HOSTNAME}/" \
+        /etc/pdns.d/*
   fi
 
   # SETUP PDNS DATABASE
   if [ ! -f "$PDNS_DATA/pdns_$(hostname).sqlite3" ]; then
-    sqlite3 $PDNS_DATA/pdns_$(hostname).sqlite3 < /root/build/powerdns-setup.sql
+      sqlite3 $PDNS_DATA/pdns_$(hostname).sqlite3 < /etc/pdns/powerdns-setup.sql
   fi
-
-  # CONFIGURE RUNIT PDNS
-  if [ ! -f "/etc/service/pdns" ]; then
-    sed -i -e "s#-PDNS_LOG-#${PDNS_LOG}#" \
-      /root/build/service/pdns/run
-  fi
-
-  # ACTIVATE RUNIT SERVICE
-  echo "* added runit pdns"
-  [ ! -d "/etc/service/pdns" ] && cp -R /root/build/service/pdns /etc/service/
 fi
