f3bd1fb2ded9596e307b63e744e794e95d6944f4
Yves Serrano <y@yas.ch>
2016-10-27 16:32:36 +0200

    only show documents with editor

--- desk/_design/desk_drawer/views/new_by_editor/map.js
+++ desk/_design/desk_drawer/views/new_by_editor/map.js
@@ -1,6 +1,6 @@
 function(doc) {
     if (doc.type != 'order') {
-        if (['new', 'changed', 'delete'].indexOf(doc.state) >= 0) {
+        if (['new', 'changed', 'delete'].indexOf(doc.state) >= 0 && doc.hasOwnProperty('editor')) {
             emit(doc.editor, doc._id);
         }
     }
