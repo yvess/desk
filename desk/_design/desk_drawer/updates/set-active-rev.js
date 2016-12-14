bd3a5c4a4a2ae2307fd996fc4a107449c2cf4d09
Yves Serrano <y@yas.ch>
2016-12-14 12:36:54 +0100

    code syntax change

--- desk/_design/desk_drawer/updates/set-active-rev.js
+++ desk/_design/desk_drawer/updates/set-active-rev.js
@@ -1,8 +1,8 @@
 function(doc, req) {
     var new_active_rev = req.query.active_rev;
     if (doc.hasOwnProperty('active_rev')) {
-        doc['prev_active_rev'] = doc['active_rev'];
+        doc.prev_active_rev = doc.active_rev;
     }
-    doc['active_rev'] = new_active_rev;
+    doc.active_rev = new_active_rev;
     return [doc, "updated active_rev to: " + new_active_rev];
 }
