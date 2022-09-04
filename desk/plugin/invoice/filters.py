18c71e914b2502cdc21588f511a5d2df4043d8b6
Yves Serrano <ys@taywa.ch>
2022-09-04 20:11:38 +0200

    fix jinja2 deprecated code

--- desk/plugin/invoice/filters.py
+++ desk/plugin/invoice/filters.py
@@ -1,15 +1,16 @@
 import re
-from jinja2 import evalcontextfilter, Markup, escape
+from jinja2 import pass_eval_context
+from jinja2.utils import markupsafe 
 
 _paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
 
 
-@evalcontextfilter
+@pass_eval_context
 def nl2br(eval_ctx, value):
     _paragraph_re = re.compile(r'(?:\r\n|\r(?!\n)|\n){2,}')
     result = '\n\n'.join('%s   ' % p.replace('\r\n', '<br/>') for p in _paragraph_re.split(value))
     if eval_ctx.autoescape:
-        result = Markup(result)
+        result = markupsafe.Markup(result)
     return result
 
 
